1d9a5da5b1dd71e61ee10b1e6e0e16dd9909e6bb
ashiqsyed <ahs57816@uga.edu>
2024-02-10 13:10:59 -0500

    created template for jobinfo page

---
+++ ComQuest/app/src/main/java/edu/uga/cs/comquest/JobInfo.java
@@ -0,0 +1,20 @@
+package edu.uga.cs.comquest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JobInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_job_info);
+
+        //customer name
+        //customer profile picture
+        //customer job description
+
+        //accept button
+    }
+}
