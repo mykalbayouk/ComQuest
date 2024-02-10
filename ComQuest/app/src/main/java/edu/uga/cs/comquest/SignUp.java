c0c461c370c77d8fb4e92bd0238bcfefbc1027a8
Michael <michaelbayouk@gmail.com>
2024-02-09 22:17:50 -0500

    sign up layout done

---
+++ ComQuest/app/src/main/java/edu/uga/cs/comquest/SignUp.java
@@ -0,0 +1,16 @@
+package edu.uga.cs.comquest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+
+
+}
