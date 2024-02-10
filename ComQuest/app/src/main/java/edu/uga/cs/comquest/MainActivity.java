378e40bb846e977b7dc65369a70a927cf00ab945
Michael <michaelbayouk@gmail.com>
2024-02-09 20:15:20 -0500

    poop

--- ComQuest/app/src/main/java/edu/uga/cs/comquest/MainActivity.java
+++ ComQuest/app/src/main/java/edu/uga/cs/comquest/MainActivity.java
@@ -3,6 +3,9 @@ package edu.uga.cs.comquest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button_i = findViewById(R.id.button_id);
+        TextView text = findViewById(R.id.text_i);
+
+        button_i.setOnClickListener((view) -> {
+            text.setText("poop");
+
+        });
     }
 }
+
